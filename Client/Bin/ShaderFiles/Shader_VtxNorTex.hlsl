f50b27ce07bd287493cae2304330df2b8734a335
Vincent ryu <bigasiler@gmail.com>
2023-10-11 23:06:02 +0900

    -엔진 로딩 화면 작업중. 파이프라인 클래스 추가. 카메라 클래스 추가. 지형 VI버퍼 추가(미사용) 현재 레벨 얻어오는 함수 추가. 카메라모드 겟셋 추가.
    -클라
    카메라모드 이넘 추가.
    캐릭터 생성 레벨 추가.
    디버그용 카메라 추가.
    노말 있는 셰이더 추가.
    서브타이틀 만들다가 말았음.

---
+++ Client/Bin/ShaderFiles/Shader_VtxNorTex.hlsl
@@ -0,0 +1,80 @@
+matrix g_WorldMatrix, g_ViewMatrix, g_ProjMatrix;
+texture2D g_Texture;
+int g_TexIndex;
+
+sampler PointSampler = sampler_state
+{
+    Filter = MIN_MAG_MIP_POINT;
+};
+
+sampler LinearSampler = sampler_state
+{
+    Filter = MIN_MAG_MIP_LINEAR;
+};
+
+RasterizerState RS_Test
+{
+    CullMode = None;
+};
+
+struct VS_IN
+{
+    float3 vPos : Position;
+    float2 vTex : Texcoord0;
+};
+
+struct VS_OUT
+{
+    vector vPos : SV_Position; // == float4
+    float2 vTex : Texcoord0;
+};
+
+VS_OUT VS_Main(VS_IN Input)
+{
+    VS_OUT Output = (VS_OUT) 0;
+	
+    vector vPosition = mul(float4(Input.vPos, 1.f), g_WorldMatrix);
+    vPosition = mul(vPosition, g_ViewMatrix);
+    vPosition = mul(vPosition, g_ProjMatrix);
+	
+    Output.vPos = vPosition;
+    Output.vTex = Input.vTex;
+	
+    return Output;
+}
+
+struct PS_IN
+{
+    vector vPos : SV_Position;
+    float2 vTex : Texcoord0;
+};
+
+struct PS_OUT
+{
+    vector vColor : SV_Target0;
+};
+
+PS_OUT PS_Main(PS_IN Input)
+{
+    PS_OUT Output = (PS_OUT) 0;
+    
+    Output.vColor = g_Texture.Sample(LinearSampler, Input.vTex);
+    
+    if (Output.vColor.a < 0.7f)
+    {
+        discard;
+    }
+
+    return Output;
+}
+
+technique11 DefaultTechnique
+{
+    pass UI
+    {
+        SetRasterizerState(RS_Test);
+
+        VertexShader = compile vs_5_0 VS_Main();
+        PixelShader = compile ps_5_0 PS_Main();
+    }
+};
